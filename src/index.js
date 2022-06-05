9e913c109b0376d937461587b13495588bbbf8d2
aayushrathor <rathoreaayush84@gmail.com>
2022-06-05 16:17:33 +0530

    response class added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import Response from './domain/response.js';
 
 dotenv.config();
 
@@ -10,6 +11,6 @@ const app = express();
 app.use(cors({origin: '*'}));
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send({message: 'Server is UP'}) });
+app.get('/', (req, res) => res.send(new Response(200, 'OK', 'Patient API, v1.0.0 - All Systems GO')));
 console.log(process.env);
 app.listen(PORT, () => console.log(`Server running on: ${ip.address()}:${PORT}`));
