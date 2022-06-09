62704fa3345bc53fe020ad6786f86c88abcd07ce
aayushrathor <rathoreaayush84@gmail.com>
2022-06-09 11:52:18 +0530

    queries added

---
+++ src/query/patient.query.js
@@ -0,0 +1,7 @@
+const QUERY = {
+	SELECT_PATIENTS: 'SELECT * FROM patients ORDER BY create_at DESC LIMIT 100',	
+	SELECT_PATIENT: 'SELECT * FROM patients WHERE ID = ?',
+	CREATE_PATIENT: 'INSERT INTO patients(first_name, email, addr, diagnose, phone, image_url) VALUES (?, ?, ?, ?, ?, ?)',
+	UPDATE_PATIENTS: 'UPDATE patients SET first_name = ?, email = ?, addr = ?, diagnose = ?, phone = ?, image_url = ? WHERE id = ?',
+	SELECT_PATIENTS: 'DELETE FROM patients WHERE id = ?'
+}
