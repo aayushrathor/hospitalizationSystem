56e382d751470085fd445088947323751ee9bdef
aayushrathor <rathoreaayush84@gmail.com>
2022-06-18 20:37:00 +0530

    delimiter create procedure

--- database/init.sql
+++ database/init.sql
@@ -16,3 +16,14 @@ CREATE TABLE patients(
 	PRIMARY KEY (id),
 	CONSTRAINT UQ_EMAIL UNIQUE (email) 
 );
+
+DELIMITER //
+CREATE PROCEDURE create_and_return(IN first_name VARCHAR(255), IN last_name VARCHAR(255), IN email VARCHAR(255), IN phone VARCHAR(255), IN diagnose VARCHAR(255), IN image_url VARCHAR(255))
+BEGIN
+	INSERT INTO patients(first_name, email, addr, diagnose, phone, image_url) VALUES (first_name, email, addr, diagnose, phone, image_url);
+
+	SET @PATIENT_ID = LAST_INSERT_ID();
+
+	SELECT * FROM patients WHERE id=@PATIENT_ID;
+END //
+DELIMITER ;
