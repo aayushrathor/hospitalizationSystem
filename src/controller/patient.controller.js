42853fc92698375da2c9815569c6f353c6d12c2f
aayushrathor <rathoreaayush84@gmail.com>
2022-06-18 13:19:28 +0530

    get create and delete functions added

--- src/controller/patient.controller.js
+++ src/controller/patient.controller.js
@@ -9,11 +9,11 @@ const HttpStatus = {
 	NO_CONTENT: { code: 204, status: 'NO_CONTENT' }, 
 	BAD_REQUEST: { code: 400, status: 'BAD_REQUEST' }, 
 	NOT_FOUND: { code: 404, status: 'NOT_FOUND' }, 
-	INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' }
+	INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' },
 };
 
 export const getPatients = (req, res) => {
-	logger.info(`${req.method} ${req.originalurl}, fetching patients`);
+	logger.info(`${req.method} ${req.originalUrl}, fetching patients`);
 	database.query(QUERY.SELECT_PATIENTS, (error, results) => {
 		if(!results) {
 			res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `No Patients Found`));
@@ -21,6 +21,85 @@ export const getPatients = (req, res) => {
 			res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patients Retrieved`, { patients: results }));
 		}
 	});
-};
+}
+
+export const createPatient = (req, res) => {
+    logger.info(`${req.method} ${req.originalUrl}, creating Patient`);
+    database.query(QUERY.CREATE_PATIENT, Object.values(req.body), (error, results) => {
+        if(!results) {
+            logger.error(error.message);
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+            .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Error occured`));
+
+        }
+        else{
+            const patient = { id: results.insertedId, ...req.body, created_at: new Date()};
+            res.status(HttpStatus.CREATED.code)
+            .send(new Response (HttpStatus.CREATED.code, HttpStatus.CREATED.status, `Patient created`, {patient} ))
+
+        }
+    })
+}
+
+
+
+export const getPatient = (req, res) => {
+    logger.info(`${req.method} ${req.originalUrl}, fetching Patient`);
+    database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results) => {
+        if(!results[0]) {
+             res.status(HttpStatus.NOT_FOUND.code)
+            .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Patient by id ${req.params.id} was not found`));
+
+        }
+        else{
+            res.status(HttpStatus.OK.code)
+            .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patient retrieved`, results[0] ))
+
+        }
+    })
+}
+
+export const updatePatient = (req, res) =>{
+    logger.info(`${req.method} ${req.originalUrl}, fetching patient`);
+    database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results)=>{
+        if(!results[0]){
+            res.status(HttpStatus.NOT_FOUND.code)
+            .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Patient by id ${req.params.id}`))
+        }
+        else{
+            logger.info(`${req.method} ${req.originalUrl}, updating patient`);
+            database.query(QUERY.UPDATE_PATIENT, [...Object.values(req.body), req.params.id], (error, results)=>{
+                if(!error){
+                    res.status(HttpStatus.OK.code)
+                    .send(new Response(HttpStatus.OK.code, HttpStatus.status, `Patient Updated`, {id : re.params.id, ...req.body}))
+                } else {
+                    logger.error(error.message);
+                    res.status(HttpStatus.NOT_FOUND.code)
+                    .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Error Occured`))
+                }
+            });
+        }
+
+    });
+
+}
+
+
+export const deletePatient = (req, res) => {
+    logger.info(`${req.method} ${req.originalUrl}, deleting patient`);
+    database.query(QUERY.DELETE_PATIENT, [req.params.id] ,(error, results)=>{
+        if(results.affectedRows > 0){
+            res.status(HttpStatus.OK.code)
+            .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patient deleted`, results[0]));
+
+        }
+        else{
+            res.status(HttpStatus.NOT_FOUND.code)
+        .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status,`Patient by id ${req.params.id} was not found` ));        }
+
+        
+    })
+}
+
 
 export default HttpStatus;
